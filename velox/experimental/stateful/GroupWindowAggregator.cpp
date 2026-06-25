acd8c3ecd1b4488fb8dd7121bd2635f02ef60220
lgbo-ustc <lgbo.ustc@gmail.com>
2026-06-25 11:02:41 +0800

    noops

--- velox/experimental/stateful/GroupWindowAggregator.cpp
+++ velox/experimental/stateful/GroupWindowAggregator.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "velox/experimental/stateful/GroupWindowAggregator.h"
 #include <cstdint>
 #include "velox/experimental/stateful/window/TimeWindowUtil.h"
